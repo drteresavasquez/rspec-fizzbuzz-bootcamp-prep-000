a11a555b0dbb861d8ca8395e8e3d54152f18d983
Teresa Terry <dr.terry2013@gmail.com>
2018-02-07 16:38:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,13 @@
 def fizzbuzz(num)
   if (num % 3 == 0 && num % 5 == 0){
     "FizzBuzz"
-    }elsif(num % 3 == 0){
-      "Fizz"
-    }elsif(num % 5 == 0){
-      "Buzz"
-    }else{
-      "Sorry! #{num} isn't divisible by 5 or 3!"
     }
+    # elsif(num % 3 == 0){
+    #   "Fizz"
+    # }elsif(num % 5 == 0){
+    #   "Buzz"
+    # }else{
+    #   "Sorry! #{num} isn't divisible by 5 or 3!"
+    # }
   end
 end
