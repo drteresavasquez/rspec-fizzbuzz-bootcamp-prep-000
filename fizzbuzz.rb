b63dedb7c79b352569fe1c66523564e9b137a92e
Teresa Terry <dr.terry2013@gmail.com>
2018-02-07 16:39:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,9 @@ def fizzbuzz(num)
     "FizzBuzz"
     elsif(num % 3 == 0)
       "Fizz"
-    }elsif(num % 5 == 0){
+    elsif(num % 5 == 0)
       "Buzz"
-    }else{
+    else
       "Sorry! #{num} isn't divisible by 5 or 3!"
-    }
   end
 end
